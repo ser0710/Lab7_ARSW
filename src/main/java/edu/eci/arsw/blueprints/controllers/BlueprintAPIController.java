4cfae18e6a0d594b7c8a02cee328cb67ca23e12c
ser0710 <serroz2822@gmail.com>
2022-09-22 09:40:00 -0500

    primer avance

---
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.controllers;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+
+/**
+ *
+ * @author hcadavid
+ */
+@RestController
+@RequestMapping(value = "/blueprints")
+
+public class BlueprintAPIController {
+
+    @Autowired
+    BlueprintsServices bps;
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public ResponseEntity<?> getBlueprint(){
+        try{
+            Set<Blueprint> data = bps.getAllBlueprints();
+            Gson gson = new Gson();
+            return new ResponseEntity<>(gson.toJson(data), HttpStatus.ACCEPTED);
+        } catch (BlueprintNotFoundException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(path = "/{author}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getBlueprintByAuthor(@PathVariable String author) throws BlueprintNotFoundException {
+        Set<Blueprint> data = bps.getBlueprintsByAuthor(author);
+        Gson gson = new Gson();
+        if(data.isEmpty()){
+            return new ResponseEntity<>("Dicho autor no existe",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(gson.toJson(data), HttpStatus.ACCEPTED);
+
+
+    }
+
+    @RequestMapping(path = "/{author}/{bpname}" , method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getBlueprintByAuthorName(@PathVariable String author, @PathVariable String bpname) throws BlueprintNotFoundException {
+            Blueprint blueprint = bps.getBlueprint(author, bpname);
+            Gson gson = new Gson();
+            if(blueprint == null){
+                return new ResponseEntity<>("Dicho nombre o autor no existe",HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(gson.toJson(blueprint), HttpStatus.ACCEPTED);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> postBlueprint(@RequestBody Blueprint blueprint){
+        try {
+            bps.addNewBlueprint(blueprint);
+            //registrar dato
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (BlueprintPersistenceException ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error bla bla bla",HttpStatus.FORBIDDEN);
+        }
+
+    }
+
+    @RequestMapping(path = "/{author}/{bpname}", method = RequestMethod.PUT,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> putBlueprint(@PathVariable String author, @PathVariable String bpname, @RequestBody Blueprint blueprint) throws BlueprintNotFoundException {
+        Blueprint blueprint1 = bps.getBlueprint(author, bpname);
+        blueprint1.setAuthor(blueprint.getAuthor());
+        blueprint1.setName(blueprint.getName());
+        blueprint1.setPoints(blueprint.getPoints());
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+}
+
---
+++ src/main/resources/static/JS/app.js
@@ -0,0 +1,30 @@
+apimock=(function(){
+
+	var mockdata=[];
+
+	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
+	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"},
+	 {author:"johnconnor","points":[{"x":34,"y":76},{"x":190,"y":71}],"name":"apto"}];
+	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
+	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"},
+	 {author:"maryweyland","points":[{"x":123,"y":89},{"x":10,"y":111}],"name":"apto2"}];
+	mockdata["EstebanQuito"] = [{author: "EstebanQuito", "points":[{"x":134,"y":86},{"x":125,"y":165}], "name": "house3"},
+	{author: "EstebanQuito", "points":[{"x":75,"y":12},{"x":210,"y":167}], "name": "gear3"}]
+
+
+	return {
+		getBlueprintsByAuthor:function(authname,callback){
+			callback(
+				mockdata[authname]
+			);
+		},
+
+		getBlueprintsByNameAndAuthor:function(authname,bpname,callback){
+
+			callback(
+				mockdata[authname].find(function(e){return e.name===bpname})
+			);
+		}
+	}
+
+})();
